89862c5ebb17d8a23c7ce6563ba1d351d99aa235
Jack <48315282+Hin97@users.noreply.github.com>
2023-09-20 00:29:56 +1000

    Setup Pixi environment

---
+++ src/routes/Snake.svelte
@@ -0,0 +1,28 @@
+<script>
+    // @ts-nocheck
+
+    // YourSvelteComponent.svelte
+    import { onMount } from "svelte";
+    import * as PIXI from "pixi.js";
+
+    let app; // PixiJS Application instance
+
+    onMount(() => {
+        // Create a PixiJS Application with a WebGL renderer
+        app = new PIXI.Application({
+            width: 605,
+            height: 480,
+            background: "#1099bb",
+        });
+
+        // Append the PixiJS Application's view (canvas) to your Svelte component
+        document.getElementById("pixi-container").appendChild(app.view);
+
+        // Your PixiJS code (e.g., creating sprites, graphics, etc.) goes here
+    });
+</script>
+
+<div id="pixi-container" />
+
+<style>
+</style>
