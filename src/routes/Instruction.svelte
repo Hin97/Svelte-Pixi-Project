e49e41e2c9460cb6230964c79382abff2e8a1a26
Jack <48315282+Hin97@users.noreply.github.com>
2023-11-13 11:36:11 +1100

    Bug fixed: Cache cause the star not showing

--- src/routes/Instruction.svelte
+++ src/routes/Instruction.svelte
@@ -8,7 +8,8 @@
         <li>Crashing with the wall or yourself will ends the game</li>
         <li>The snake will get additional segement when it collects a star</li>
         <li>Collecting as much stars as possible(each difficuty has its own records)</li>
-        <li>The difficuty will impact the movement of the snake</li>
+        <li>The difficuty will impact the movement speed of the snake</li>
+        <li>If your game cannot be display properly, please clear your cache for this web application</li>
         <li style="color: red;">Close of refresh this webpage will lose all your game records</li>
     </ul>
 </div>
